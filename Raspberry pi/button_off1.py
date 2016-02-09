ccca2d3bd8bbf4eae02ea87cb9524e9cec8f355a
Karthik Bandi <karthikbandi1991@gmail.com>
2016-02-09 10:45:33 +0100

    Update button_off1.py

--- Raspberry pi/button_off1.py
+++ Raspberry pi/button_off1.py
@@ -9,9 +9,9 @@ def loop():
 # Define a function to run when an interrupt is called
 def shutdown(pin):
     call('halt', shell=False)
-
-gpio.setmode(gpio.BOARD) # Set pin numbering to board numbering
-gpio.setup(11, gpio.IN) # Set up pin 11 as an input
-gpio.add_event_detect(11, gpio.RISING, callback=shutdown, bouncetime=200) # Set up an interrupt to look for button presses
+Button_PIN=17
+gpio.setmode(gpio.BCM) # Set pin numbering to BCM numbering
+gpio.setup(Button_PIN, gpio.IN) # Set up pin 11 as an input
+gpio.add_event_detect(Button_PIN, gpio.RISING, callback=shutdown, bouncetime=200) # Set up an interrupt to look for button presses
 
 loop() # Run the loop function to keep script running
